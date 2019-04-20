9a225716e7d31395bf93e0a6986edf539245edac
Matt Bentrovato <mbentrovato@atb.com>
2019-04-20 16:42:28 -0600

    Set up for GoogleAuth component - for Login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
